adfef2714780870a85401cdaa3968461ecb08c2c
Oscar Merce <omerce@naradarobotics.com>
2017-10-05 19:03:17 +0200

    Integrating react-redux and react-router.

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import App from './app'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {AppContainer} from 'react-hot-loader';
 
-ReactDOM.render(
-  <AppContainer>
-    <App/>
-  </AppContainer>,
-  document.getElementById('app')
+import App from './app';
+import store from './store';
+
+const render = Component => (
+    ReactDOM.render(
+        <AppContainer>
+            <Provider store={store}>
+                <Component/>
+            </Provider>
+        </AppContainer>, document.getElementById('app'))
 );
 
-// Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept('./app', () => {
-    const NextApp = require('./app').default;
-    ReactDOM.render(
-      <AppContainer>
-        <NextApp/>
-      </AppContainer>,
-      document.getElementById('app')
-    );
-  });
-}
+render(App);
+if (module.hot)  module.hot.accept('./app', () => render(App));
